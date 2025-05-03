43b04a6f16a7f584e9254470f03ac7cdb50242e4
Oxdeception <30361731+Oxdeception@users.noreply.github.com>
2025-05-03 18:36:13 -0400

    Adds a typing indicator to the AAC (#3595)
    * Adds a typing indicator to the AAC
    
    * Removes duplicate TypingChangedEvent constructor
    
    * Updates sprite attribution
    
    * Corrects fork addition comment style
    
    * Moves typing indicator override from Appearance to TypingIndicatorComponent
    
    * Reduces retrievals of TypingIndicatorSystem in the AAC BUI
    
    * Moves ClientAlternateTyping to _DV
    
    * Adds missing comment
    
    * Sets TypingIndicatorComponent Dirty
    
    * Caches ShowTypingIndicator in AlternateTypingIndicator
    
    * Removes unnecessary default assignment
    
    ---------
    
    Co-authored-by: Oxdeception <9624-oxdeception@users.noreply.gitgud.io>

--- Content.Client/DeltaV/AACTablet/UI/AACBoundUserInterface.cs
+++ Content.Client/DeltaV/AACTablet/UI/AACBoundUserInterface.cs
@@ -1,3 +1,5 @@
+using Content.Client.Chat.TypingIndicator;
+using Content.Shared.Chat.TypingIndicator;
 using Content.Shared.DeltaV.AACTablet;
 using Content.Shared.DeltaV.QuickPhrase;
 using Robust.Client.UserInterface;
@@ -10,6 +12,10 @@ public sealed class AACBoundUserInterface : BoundUserInterface
     [ViewVariables]
     private AACWindow? _window;
 
+    private static readonly ProtoId<TypingIndicatorPrototype> AACTypingIndicator = "aac";
+
+    private TypingIndicatorSystem? _typing;
+
     public AACBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
     }
@@ -20,10 +26,24 @@ public sealed class AACBoundUserInterface : BoundUserInterface
         _window?.Close();
         _window = this.CreateWindow<AACWindow>();
         _window.PhraseButtonPressed += OnPhraseButtonPressed;
+        _window.Typing += OnTyping;
+        _window.SubmitPressed += OnSubmit;
     }
 
     private void OnPhraseButtonPressed(List<ProtoId<QuickPhrasePrototype>> phraseId)
     {
         SendMessage(new AACTabletSendPhraseMessage(phraseId));
+    }
+
+    private void OnTyping()
+    {
+        _typing ??= EntMan.System<TypingIndicatorSystem>();
+        _typing?.ClientAlternateTyping(AACTypingIndicator);
+    }
+
+    private void OnSubmit()
+    {
+        _typing ??= EntMan.System<TypingIndicatorSystem>();
+        _typing?.ClientSubmittedChatText();
     }
 }
