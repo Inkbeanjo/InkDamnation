ee9b4129d09a21c5a5fd48a7041d80062e570813
AssignedGod <79281911+assignedgod@users.noreply.github.com>
2025-11-04 19:05:36 -0500

    Integration tests

--- Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
+++ Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
@@ -24,7 +24,7 @@ public sealed class GuideEntryPrototypeTests
         var parser = client.ResolveDependency<DocumentParsingManager>();
         var prototypes = protoMan.EnumeratePrototypes<GuideEntryPrototype>().ToList();
 
-        foreach (var proto in prototypes)
+        foreach (var proto in prototypes.Take(5)) //Augh
         {
             await client.WaitAssertion(() =>
             {
@@ -34,7 +34,8 @@ public sealed class GuideEntryPrototypeTests
             });
 
             // Avoid styleguide update limit
-            await client.WaitRunTicks(10);
+
+            await client.WaitRunTicks(1);
         }
 
         await pair.CleanReturnAsync();
